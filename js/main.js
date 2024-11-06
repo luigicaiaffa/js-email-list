da990c28e89b374ae381a4d60d9d6564eeaa8a94
luigicaiaffa <luigicaiaffa8@gmail.com>
2024-11-06 15:07:48 +0100

    add html + css

--- js/main.js
+++ js/main.js
@@ -1,15 +1,24 @@
 // # Funzione
 
-const generateRandomMail = () => {
-  for (let i = 0; i < 10; i++) {
+const generateRandomMails = (mailNum) => {
+  for (let i = 0; i < mailNum; i++) {
     fetch("https://flynn.boolean.careers/exercises/api/random/mail")
       .then((response) => response.json())
       .then((data) => {
-          mailList.innerHTML += `<li>${data.response}</li>`;
-          console.log(data.response);
+        mailList.innerHTML += `<li class="list-group-item list-group-item-success">${data.response}</li>`;
+        console.log(data.response);
       });
   }
 };
 
+// # Recupero eleemnti html
 const mailList = document.getElementById("mail-list");
-generateRandomMail();
+const changeMailsBtn = document.getElementById("change-mails-btn");
+
+// # Output
+generateRandomMails(10);
+
+// # Bonus
+
+
+
